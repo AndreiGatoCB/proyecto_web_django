2a4224d3a72a2968fdf2917664b8a38699f9f8a7
AndreiGatoCB <baalbornozl@unal.edu.co>
2025-11-26 15:55:31 -0300

    Add image viewer functionality
    Agregué la opción para mostrar las imágenes agregadas al proyecto

--- proyecto_web_app/urls.py
+++ proyecto_web_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from proyecto_web_app import views
+
 
 urlpatterns = [
     path('', views.home, name="Home"),
@@ -8,5 +10,6 @@ urlpatterns = [
     path('tienda/', views.tienda, name="Tienda"),
     path('blog/', views.blog, name="Blog"),
     path('contacto/', views.contacto, name="Contacto"),
-
 ]
+
+urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
