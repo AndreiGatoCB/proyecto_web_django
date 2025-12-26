440156ab0cc1594cdee206a2952eb06126be841b
AndreiGatoCB <baalbornozl@unal.edu.co>
2025-12-26 14:42:07 -0300

    update app tienda
    modifiqué los campos del modelo producto, creé la conexión entre los archivos models y views de la app tienda

--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from tienda.models import *
+from tienda.models import Producto
 
 # Create your views here.
 
 
 def tienda(request):
-    # tienda_ = Tienda.objects.all()
-    return render(request, "tienda/tienda.html")#, #{"tienda": tienda_})
+    tienda_ = Producto.objects.all()
+    return render(request, "tienda/tienda.html", {"tienda": tienda_})
+
+
+def producto(request, producto_id):
+    producto = Producto.object.get(id=producto_id)
+    return render(request, "tienda/tienda.html", {"producto": producto})
