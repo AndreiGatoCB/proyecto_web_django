37a11df30749580613ad086c060725ce4ac9f967
AndreiGatoCB <baalbornozl@unal.edu.co>
2026-01-05 19:58:17 -0300

    update tienda
    Agregué cards a la tienda para separar los productos de forma visualmente agradable

--- tienda/models.py
+++ tienda/models.py
@@ -24,10 +24,9 @@ class Producto(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
-        verbose_name = "Producto"
-        verbose_name_plural = "Productos"
+        verbose_name = "producto"
+        verbose_name_plural = "productos"
 
     def __str__(self):
         return self.nombre
--- tienda/views.py
+++ tienda/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from tienda.models import Producto
 
+
 # Create your views here.
-
-
 def tienda(request):
     tienda_ = Producto.objects.all()
-    return render(request, "tienda/tienda.html", {"tienda": tienda_})
-
-
-def producto(request, producto_id):
-    producto = Producto.object.get(id=producto_id)
-    return render(request, "tienda/tienda.html", {"producto": producto})
+    return render(request, "tienda/tienda.html", {"productos": tienda_})
