61ba118ea33c55f12e15ce6d9ea2aaec2764d1bb
AndreiGatoCB <baalbornozl@unal.edu.co>
2026-02-03 19:39:02 -0300

    create app pedidos
    creé la aplicación pedidos e implementé los modelos de la misma en el archivo models.py.

---
+++ pedidos/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import F, Sum, FloatField
+
+from tienda.models import Producto
+
+User = get_user_model()
+
+
+# Create your models here.
+class Pedido(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
+
+    @property
+    def total(self):
+        return self.lineapedido_set.aggregate(
+            total = sum(F('precio')*F('cantidad'), output_field=FloatField())
+        )['total']
+
+    class Meta:
+        db_table = "pedidos"
+        verbose_name = "pedido"
+        verbose_name_plural = "pedidos"
+        ordering = ['id']
+
+
+class LineaPedido(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    pedido_id = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.cantidad} unidades de {self.producto_id.nombre}'
+
+    class Meta:
+        db_table = "lineapedidos"
+        verbose_name = "linea pedido"
+        verbose_name_plural = "lineas pedidos"
+        ordering = ['id']
