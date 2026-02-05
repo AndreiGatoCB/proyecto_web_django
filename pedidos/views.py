ba55518c1a0c9f93aac9c224b02afbbd8a1a61d5
AndreiGatoCB <baalbornozl@unal.edu.co>
2026-02-05 15:21:03 -0300

    update app pedidos
    Agregué lo necesario para las views de la aplicación.

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from carro.carro import Carro
+from pedidos.models import Pedido, LineaPedido
+
 
 # Create your views here.
+@login_required(login_url="/autenticacion/logear")
+def procesar_pedido(request):
+    pedido = Pedido.objects.create(user=request.user)
+    carro = Carro(request)
+    lineas_pedido = list()
+    for key, value in carro.carro.items():
+        lineas_pedido.append(LineaPedido(
+            producto_id=key,
+            cantidad=value("cantidad"),
+            user=request.user,
+            pedido=pedido
+        ))
+    LineaPedido.objects.bulk_create(lineas_pedido)
+
+    enviar_mail(
+        pedido=pedido,
+        lineas_pedido=lineas_pedido,
+        nombre_usuario=request.username,
+        email_usuario=request.username,
+    )
+    messages.success(request, "El pedido se ha creado correctamente.")
+
+    return redirect("../tienda")
